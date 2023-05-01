cbf9d6054a06f502b93f0f962ef1850bf28414c8
ToastyyX <erick.cool321@icloud.com>
2023-04-30 20:58:54 -0500

    7th Commit: Basic Code Refinement

--- src/CurbsidePickupDriver.java
+++ src/CurbsidePickupDriver.java
@@ -12,6 +12,7 @@ public class CurbsidePickupDriver {
         // Which essentially runs the main task of simulating a simple but "real life" curbside pickup system.
         CurbsidePickupSystem curbsidePickupSystem = new CurbsidePickupSystem(); // CurbsidePickupSystem Object
 
+
         boolean tryAgain = true;
         System.out.println("Welcome to the Toasty Hardware shop!");
         System.out.println("----------------------------------");
@@ -23,18 +24,17 @@ public class CurbsidePickupDriver {
                     System.out.print("Menu - Search Order --> 1 | Exit  --> 2 |: ");
                     userInput = stdIn.nextInt();
                     if (userInput == 1) {
-                        System.out.print("\nSearch Order Number: ");
+                        System.out.print("Search Order Number: ");
                         curbsidePickupSystem.search(stdIn.nextInt());
                     } else if (userInput == 2) {
                         curbsidePickupSystem.exit();
                     } else {
-                        System.out.println("\nInvalid Command.");
-                        System.out.println();
+                        System.out.println("Invalid Command." + "\n");
                     }
                 } while (userInput < 1 || userInput > 2);
                 tryAgain = false;
             } catch (InputMismatchException inputMismatchException) {
-                System.out.println("Invalid Input.");
+                System.out.println("Invalid Input." + "\n");
                 stdIn.next(); // consume the invalid input
             }
         }
