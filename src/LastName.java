e8f2823fdfeade3d72ac9adfe428ddbed41243b9
ToastyyX <erick.cool321@icloud.com>
2023-05-01 12:46:59 -0500

    Initial Commit: Name Generator

---
+++ src/LastName.java
@@ -0,0 +1,32 @@
+public class LastName {
+    private final String[] NAMES = {
+            "Amelia",
+            "Benjamin",
+            "Chloe",
+            "David",
+            "Eleanor",
+            "Felix",
+            "Grace",
+            "Henry",
+            "Isla",
+            "Jack",
+            "Keira",
+            "LeMi",
+            "Noah",
+            "Olivia",
+            "Pepe",
+            "Quinn",
+            "Ruby",
+            "Samuel",
+            "Tessa"
+    };
+
+    public String[] getAllNames() {
+        return NAMES;
+    }
+
+    public String getRandomName() {
+        int index = (int) (Math.random() * NAMES.length);
+        return NAMES[index];
+    }
+}
