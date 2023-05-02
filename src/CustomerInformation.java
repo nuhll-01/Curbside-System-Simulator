ac78bdc4df1d91a818511ef99cf49afd9598c675
ToastyyX <erick.cool321@icloud.com>
2023-05-02 16:17:11 -0500

    8th Commit: Improved the dynamic characteristics of customer information.

--- src/CustomerInformation.java
+++ src/CustomerInformation.java
@@ -1,29 +1,41 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 
 public class CustomerInformation extends Customer {
-    FirstName firstName = new FirstName();
-    LastName lastName = new LastName();
+    private final FirstName firstName = new FirstName();
+    private final LastName lastName = new LastName();
     private final ArrayList<Customer> customer = new ArrayList<>(); // ArrayList of customers.
 
     protected void listOfCustomers() {
-        customer.add(new Customer(firstName.getRandomName(), lastName.getRandomName(), "5555 S 10th St",
-                "WadzWorth53@domain.com", getPhoneNumber()));
-        customer.add(new Customer(firstName.getRandomName(), lastName.getRandomName(), "5555 S 63rd St",
-                "pickle555@domain.com", getPhoneNumber()));
-        customer.add(new Customer(firstName.getRandomName(), lastName.getRandomName(), "5555 s 75th St",
-                "Erin53@example.com", getPhoneNumber()));
-        customer.add(new Customer(firstName.getRandomName(), lastName.getRandomName(), "5555 s 24th St",
-                "Myers71@example.com", getPhoneNumber()));
-        customer.add(new Customer(firstName.getRandomName(), lastName.getRandomName(), "5555 s 56th St",
-                "Jose53@example", getPhoneNumber()));
+        /* We generate a random first name and store it onto the variable "firstName."
+           We do this because we want to use that same first name for the customer email.
+           If were to exclude the action storing the value of the generated name onto a variable,
+           We would end up with two different instances of random names therefore causing the customer email to be different from the customers name.
+         */
+        String firstName = this.firstName.generateRandomName();
+        customer.add(new Customer(firstName, lastName.getRandomName(), "5555 S 10th St",
+                getEmail(firstName.toLowerCase()), getPhoneNumber()));
+        customer.add(new Customer(firstName, lastName.getRandomName(), "5555 S 63rd St",
+                getEmail(firstName.toLowerCase()), getPhoneNumber()));
+        customer.add(new Customer(firstName, lastName.getRandomName(), "5555 s 75th St",
+                getEmail(firstName.toLowerCase()), getPhoneNumber()));
+        customer.add(new Customer(firstName, lastName.getRandomName(), "5555 s 24th St",
+                getEmail(firstName.toLowerCase()), getPhoneNumber()));
+        customer.add(new Customer(firstName, lastName.getRandomName(), "5555 s 56th St",
+                getEmail(firstName.toLowerCase()), getPhoneNumber()));
     }
 
     // This method generates a random phone number.
-    private String randomPhoneNumber() {
+    private @NotNull String randomPhoneNumber() {
         int digitLength = 10; // Max length of the phone number.
         long generateNumber = (long) (Math.random() * Math.pow(10, digitLength)); // Generates a random number.
         String generatedNumber = String.valueOf(generateNumber); // Converts the long to a string.
         return generatedNumber.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3"); // Formats the string.
+    }
+
+    public String getEmail(String firstName) {
+        return firstName + "@example.com";
     }
 
     public String getPhoneNumber() {
