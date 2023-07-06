f17f87f94b5987931da56610c159ad493447982a
ToastyyX <erick.cool321@icloud.com>
2023-07-06 13:50:04 -0500

    11th Commit: Additional Functionality + Code Rearrangement

--- src/CustomerInformation.java
+++ src/CustomerInformation.java
@@ -1,40 +1,9 @@
 import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
+import java.util.Random;
 
 public class CustomerInformation extends Customer {
-    private final ArrayList<Customer> customer = new ArrayList<>(); // ArrayList of customers.
-
-    /* We generate a random first name and store it onto the variable "firstName."
-       We do this because we want to use that same first name for the customer email.
-       If were to exclude the action of storing the value of the generated name onto a variable,
-       We would end up with two different instances of random names therefore causing the customer email to be different from the customers name.
-    */
-    protected void listOfCustomers() {
-        String firstName = generateFName();
-        customer.add(new Customer(firstName, generateLName(), "5555 S 10th St",
-                getEmail(firstName.toLowerCase()), getPhoneNumber()));
-        customer.add(new Customer(firstName, generateLName(), "5555 S 63rd St",
-                getEmail(firstName.toLowerCase()), getPhoneNumber()));
-        customer.add(new Customer(firstName, generateLName(), "5555 s 75th St",
-                getEmail(firstName.toLowerCase()), getPhoneNumber()));
-        customer.add(new Customer(firstName, generateLName(), "5555 s 24th St",
-                getEmail(firstName.toLowerCase()), getPhoneNumber()));
-        customer.add(new Customer(firstName, generateLName(), "5555 s 56th St",
-                getEmail(firstName.toLowerCase()), getPhoneNumber()));
-    }
-
-    // Generates a random number.
-    private @NotNull String randomPhoneNumber() {
-        int digitLength = 10; // Max length of the phone number.
-        long generateNumber = (long) (Math.random() * Math.pow(10, digitLength)); // Generates a random number.
-        String generatedNumber = String.valueOf(generateNumber); // Converts the long to a string.
-        return generatedNumber.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3"); // Formats the string.
-    }
-
-    public @NotNull String getPhoneNumber() {
-        return randomPhoneNumber();
-    }
-
     private final String[] FIRST_NAMES = {
             "Amelia",
             "Benjamin",
@@ -56,7 +25,6 @@ public class CustomerInformation extends Customer {
             "Samuel",
             "Tessa"
     };
-
     private final String[] LAST_NAMES = {
             "Parker",
             "Garcia",
@@ -79,9 +47,48 @@ public class CustomerInformation extends Customer {
             "Davis",
     };
 
+    /*
+    We generate a random first name and store it onto the variable "firstName."
+    We do this because we want to use that same first name for the customer email.
+    If were to exclude the action of storing the value of the generated name onto a variable,
+    We would end up with two different instances of random names therefore causing the customer email to be different from the customers name.
+    */
+
+    private final Random random = new Random();
+    private final ArrayList<Customer> customer = new ArrayList<>(); // ArrayList of customers.
+
+    public CustomerInformation() {
+        super();
+    }
+
+    protected void listOfCustomers() {
+        customer.add(new Customer(generateFName(), generateLName(), generateAddress(),
+                getEmail(generateFName().toLowerCase()), getPhoneNumber()));
+        customer.add(new Customer(generateFName(), generateLName(), generateAddress(),
+                getEmail(generateFName().toLowerCase()), getPhoneNumber()));
+        customer.add(new Customer(generateFName(), generateLName(), generateAddress(),
+                getEmail(generateFName().toLowerCase()), getPhoneNumber()));
+        customer.add(new Customer(generateFName(), generateLName(), generateAddress(),
+                getEmail(generateFName().toLowerCase()), getPhoneNumber()));
+        customer.add(new Customer(generateFName(), generateLName(), generateAddress(),
+                getEmail(generateFName().toLowerCase()), getPhoneNumber()));
+    }
+
+    // Generates a random number.
+    private @NotNull String generateNumber() {
+        int digitLength = 10; // Max length of the phone number.
+        long generateNumber = (long) (Math.random() * Math.pow(10, digitLength)); // Generates a random number.
+        String generatedNumber = String.valueOf(generateNumber); // Converts the long to a string.
+        return generatedNumber.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3"); // Formats the string.
+    }
+
     private @NotNull String generateFName() {
         int index = (int) (Math.random() * FIRST_NAMES.length);
         return FIRST_NAMES[index];
+    }
+
+    public @NotNull String generateAddress() {
+        return houseNumber() + " " + cardinalDirection() + " " + streetNumber() + "St " + zipCode();
     }
 
     private @NotNull String generateLName() {
@@ -89,15 +96,38 @@ public class CustomerInformation extends Customer {
         return LAST_NAMES[index];
     }
 
+    public @NotNull String getPhoneNumber() {
+        return generateNumber();
+    }
 
     public String getEmail(String firstName) {
         return firstName + "@example.com";
     }
 
+    public @NotNull String houseNumber() {
+        int fourDigitNumber = random.nextInt(0, 10000);
+        return String.valueOf(fourDigitNumber);
+    }
+
+    public @NotNull String cardinalDirection() {
+        String[] cardinalDirection = {"N", "S", "E", "W"};
+        int index = (int) (Math.random() * cardinalDirection.length);
+        return cardinalDirection[index];
+    }
+
+    public @NotNull String streetNumber() {
+        int streetNumb = random.nextInt(0, 100); // Generates a random number between 0 and 100.
+        return String.valueOf(streetNumb);
+    }
+
+    public @NotNull String zipCode() {
+        int zipCode = random.nextInt(0, 10000); // Generates a random number between 0 and 10000.
+        return String.valueOf(zipCode);
+    }
+
     public Customer getCustomer(int index) {
         return customer.get(index);
     }
-
 
     private String[] getAllFirstNames() {
         return FIRST_NAMES;
