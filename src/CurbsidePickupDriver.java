b12f5362d4e2740fe83de02d6dd677f95bcd1dc7
ToastyyX <erick.cool321@icloud.com>
2023-07-06 13:27:21 -0500

    12th Commit: Visual Changes + Loop Unrolling Technique

--- src/CurbsidePickupDriver.java
+++ src/CurbsidePickupDriver.java
@@ -12,25 +12,28 @@ public class CurbsidePickupDriver {
 
         int userInput, orderNumber;
         boolean tryAgain = true;
-        System.out.println("Welcome to the Toasty Hardware shop!");
-        System.out.println("------------------------------------");
-
+        System.out.println("""
+                \n====================================
+                Welcome to the Toasty Hardware shop!
+                ====================================
+                """);
         while (tryAgain) {
             try {
                 do {
                     System.out.print("Menu - Search Order --> 1 | Exit  --> 2 |: ");
                     userInput = stdIn.nextInt();
                     if (userInput == 1) {
-                        System.out.print("Search Order Number: ");
-                        orderNumber = stdIn.nextInt();
-                        for (int i = 0; i < 1; i++) {
-                            System.out.println("\nSearching...");
-                            Thread.sleep(1000);
-                            System.out.println("Retrieving Order...");
-                            Thread.sleep(1000);
-                        }
+                        do {
+                            System.out.print("Search Order Number: ");
+                            orderNumber = stdIn.nextInt();
+                        } while (curbsidePickupSystem.checkLength(orderNumber));
+                        System.out.println("\nSearching...");
+                        Thread.sleep(1000);
+                        System.out.println("Retrieving Order...");
+                        Thread.sleep(1000);
                         curbsidePickupSystem.search(orderNumber);
                     } else if (userInput == 2) {
+                        System.out.println("\nProgram Terminated.");
                         curbsidePickupSystem.exit();
                     } else {
                         System.out.println("Invalid Command." + "\n");
