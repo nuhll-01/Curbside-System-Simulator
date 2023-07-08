c7c10acc337add5bbd07d22d085aabeb89df6bd1
ToastyyX <erick.cool321@icloud.com>
2023-07-07 23:55:01 -0500

    Initial Commit: JUnit Testing

---
+++ Test/CurbsidePickupSystemTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Assert;
+
+public class CurbsidePickupSystemTest {
+    @Test
+    public void checkNumber() {
+        assertEquals(123, 123);
+    }
+}
