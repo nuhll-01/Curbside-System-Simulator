26fdf25b381161926bbb49d4de715facb56476e8
ToastyyX <erick.cool321@icloud.com>
2023-07-08 23:44:00 -0500

    2nd Commit: Added Tests

--- Test/CurbsidePickupSystemTest.java
+++ Test/CurbsidePickupSystemTest.java
@@ -1,10 +1,30 @@
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
-import org.junit.Assert;
+import static org.junit.Assert.fail;
 
 public class CurbsidePickupSystemTest {
     @Test
     public void checkNumber() {
         assertEquals(123, 123);
+    }
+
+    @Test
+    public void incompleteTest() { // This test will fail.
+        fail("Not yet completed");
+    }
+
+
+    public int randomInteger() {
+        return (int) (Math.random() * 6);
+    }
+
+    @Test
+    public void testingCondition() {
+        int number = 5;
+        if (randomInteger() != number) {
+            fail("The number is not 5");
+        }
+        // more testing code
     }
 }
